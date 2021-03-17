71acd7610fa37900268862eba629b81c3d0f22fd
Sai Pradeepti <saipradeepti14@gmail.com>
2021-03-17 13:01:05 +0530

    Adding data

--- 01twitter-sidebar-clone/src/App.js
+++ 01twitter-sidebar-clone/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTwitter} from "@fortawesome/free-brands-svg-icons";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 const App = () => {
   return (
     <>
-      
+      {/* <FontAwesomeIcon icon={fa-twitter} /> */}
+      <FontAwesomeIcon icon={faTwitter}/>
     </>
   )
 }
---
+++ 01twitter-sidebar-clone/src/data.js
@@ -0,0 +1,42 @@
+export default const data = [
+    {   
+        icon: 'home',
+        tabName: 'Home',    
+        url: 'https://twitter.com/home'
+    },
+    {   
+        icon: 'hashtag',
+        tabName: 'Explore',    
+        url: 'https://twitter.com/explore'
+    },
+    {
+        icon: 'bell',
+        tabName: 'Notifications',    
+        url: 'https://twitter.com/notifications'
+    }, 
+    {
+        icon: 'envelope',
+        tabName: 'Messages',    
+        url: 'https://twitter.com/messages'
+    }, 
+    {
+        icon: 'bookmark',
+        tabName: 'Bookmarks',    
+        url: 'https://twitter.com/i/bookmarks'
+    }, 
+    {
+        icon: 'list',
+        tabName: 'Lists',    
+        url: 'https://twitter.com/s_pradeepti/lists'
+    }, 
+    {
+        icon: 'user-circle',
+        tabName: 'Profile',    
+        url: 'https://twitter.com/s_pradeepti'
+    }, 
+    {
+        icon: 'ellipsis-h',
+        tabName: 'More',    
+        url: ''
+    }, 
+];
