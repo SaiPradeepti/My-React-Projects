e161b42381adfa9d76c7a4b3ac26028793aae6d2
Sai Pradeepti <saipradeepti14@gmail.com>
2021-03-17 23:05:29 +0530

    Adding fontawesome icons

--- 01twitter-sidebar-clone/src/App.js
+++ 01twitter-sidebar-clone/src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {data} from './data'
 import Tab from './Tab'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faTwitter} from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+import FontAwesome from 'react-fontawesome'
+import faStyles from 'font-awesome/css/font-awesome.css'
 
 const App = () => {
   return (
     <div className="sidebar">
-      <FontAwesomeIcon icon={faTwitter}/>
+      <FontAwesome name='twitter'/>
       {
         data.map((tabDetails) => {      
           return <Tab {...tabDetails}/>
--- 01twitter-sidebar-clone/src/data.js
+++ 01twitter-sidebar-clone/src/data.js
@@ -1,40 +1,41 @@
+import { FaTwitter,FaHome,FaHashtag,FaRegBell,FaEllipsisH } from "react-icons/fa";
 export const data = [
-    {   
-        icon: 'home',
+    {   id:1,
+        icon: "home",
         tabName: 'Home',    
         url: 'https://twitter.com/home'
     },
-    {   
+    {   id:2,
         icon: 'hashtag',
         tabName: 'Explore',    
         url: 'https://twitter.com/explore'
     },
-    {
+    {   id:3,
         icon: 'bell',
         tabName: 'Notifications',    
         url: 'https://twitter.com/notifications'
     }, 
-    {
+    {   id:4,
         icon: 'envelope',
         tabName: 'Messages',    
         url: 'https://twitter.com/messages'
     }, 
-    {
+    {   id:5,
         icon: 'bookmark',
         tabName: 'Bookmarks',    
         url: 'https://twitter.com/i/bookmarks'
     }, 
-    {
+    {   id:6,
         icon: 'list',
         tabName: 'Lists',    
         url: 'https://twitter.com/s_pradeepti/lists'
     }, 
-    {
+    {   id:7,
         icon: 'user-circle',
         tabName: 'Profile',    
         url: 'https://twitter.com/s_pradeepti'
     }, 
-    {
+    {   id:8,
         icon: 'ellipsis-h',
         tabName: 'More',    
         url: ''
