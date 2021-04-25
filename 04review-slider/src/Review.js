92d9097d09ace9c9939553ec0280cca5c778c1b6
Sai Pradeepti <saipradeepti14@gmail.com>
2021-04-25 14:15:15 +0530

    Adding AutoPlay feature

--- 04review-slider/src/Review.js
+++ 04review-slider/src/Review.js
@@ -9,64 +9,79 @@ const Review = () => {
   
   const [index,setIndex] = useState(0);
   const [animate,setAnimate] = useState(false)
-  const { image, name, title, quote } = people[index];
+  // const { image, name, title, quote } = people[index];
 
-  const prevPerson = () => {
-      let newIndex;
-      setIndex((index) => {
-        newIndex = index - 1;
-        if(newIndex < 0)
-          newIndex = people.length - 1;          
-        return newIndex;
-      });
-      setAnimate(!animate)
-  }
-  const nextPerson = () => {
-      let newIndex;
-      setIndex((index) => {
-        newIndex = index + 1;
-        if(newIndex > people.length - 1)
-          newIndex = 0;
-        return newIndex;
-      });
-      setAnimate(!animate)
-  }
+  useEffect(() => {
+    if(index < 0){
+      setIndex(people.length -1)
+    }
+    if(index > people.length - 1){
+      setIndex(0)
+    }
+  }, [index,people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index+1)
+    },3000);
+    return () => {
+      clearInterval(slider)
+    }
+  }, [index])
   
   useEffect(() => {
     setAnimate(!animate)
   }, [index])
 
-    return (
-        <div className="review">
-          <div className="review__title">Our Reviews</div>
-            <div className="image__container">
-                <img src={image} alt="" className={animate ? 'animate__animated animate__fadeIn' : ''}/>
-            </div>
-            <div className="reviewer">
-                <div className="reviewer__name">{name}</div>
-                <div className="reviewer__title">{title}</div>
-                <div className="review__quote">
-                    <div className="quote-left">
-                      <FontAwesomeIcon icon='quote-left' />
-                    </div>
-                    <div className="reviewer__quote">
-                      {quote}
-                    </div>
-                    <div className="quote-right">
-                      <FontAwesomeIcon icon='quote-right' />
-                    </div>
+  return (
+    <>
+      {
+        people.map((person,personIndex) => {
+          const {id, image, name, title, quote } = person;
+          let position = 'nextSlide';
+          if(index === personIndex){
+            position = 'activeSlide'
+          }
+          if(personIndex === index - 1 || (index === 0 && personIndex === people.length -1)){
+            position = 'lastSlide'
+          }
+          return (
+            <div key={id} className={`review ${position}`}>
+                <div className="review__title">Our Reviews</div>
+                <div className="reviewer__container">
+                  <div className="image__container">
+                      <img src={image} alt="" className={animate ? 'animate__animated animate__fadeIn' : ''}/>
+                  </div>
+                  <div className="reviewer">
+                      <div className="reviewer__name">{name}</div>
+                      <div className="reviewer__title">{title}</div>
+                      <div className="review__quote">
+                          <div className="quote-left">
+                            <FontAwesomeIcon icon='quote-left' />
+                          </div>
+                          <div className="reviewer__quote">
+                            {quote}
+                          </div>
+                          <div className="quote-right">
+                            <FontAwesomeIcon icon='quote-right' />
+                          </div>
+                      </div>
+                  </div>            
                 </div>
-            </div>
-            <div className="btn__container">
-                <button className="prev__btn" onClick={prevPerson}>
-                  <FontAwesomeIcon icon='arrow-left' />
-                </button>
-                <button className="next__btn">
-                  <FontAwesomeIcon icon='arrow-right' onClick={nextPerson}/>                    
-                </button>
-            </div>
-        </div>
-    )
+                  <div className="btn__container">
+                      <button className="prev__btn" onClick={() => setIndex(index-1)}>
+                        <FontAwesomeIcon icon='arrow-left' />
+                      </button>
+                      <button className="next__btn" onClick={() => setIndex(index+1)}>
+                        <FontAwesomeIcon icon='arrow-right' />                    
+                      </button>
+                  </div>
+              </div>
+          )
+        })
+      }
+    </>
+  )
 }
 
 export default Review
