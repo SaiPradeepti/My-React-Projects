482c24b55f54b0f469b9be237b30228026e2ccf3
Sai Pradeepti <saipradeepti14@gmail.com>
2021-06-16 14:38:25 +0530

    Adding Empty Cart msg to Cart Component

--- 09-shopzone/src/components/Cart.js
+++ 09-shopzone/src/components/Cart.js
@@ -10,6 +10,17 @@ const Cart = () => {
         dispatch({type: 'calTotalAmount'});
     },[cart])
 
+    // if(cart.length === 0){
+    //     return (
+    //         <div className={showCart?'cart':'cart hide-cart'}>
+    //             <div className="emtpyCart-msg">
+    //                 your cart is empty!
+    //             </div>
+    //         </div>
+            
+    //     )
+    // }
+
     return (
         <div className={showCart?'cart':'cart hide-cart'}>
             <div className="cart__title">
@@ -18,44 +29,55 @@ const Cart = () => {
             <div className="cart__remove" onClick={()=>dispatch({type:'toggleCart'})}>
                 <AiOutlineCloseCircle />
             </div>
-            <div className="cart__details">
-                {
-                    cart.map(item => {
-                        const {id, title, price, count} = item;
-                        return (
-                            <div className="section" key={id}>
-                                <div className="section__title">
-                                    {title}
-                                </div>
-                                <div className="section__price">
-                                    $ {price}
-                                </div>
-                                <div className="section__removeItem" onClick={()=>dispatch({type:'removeFromCart',payload: {id}})}>
-                                   remove
-                                </div>
-                                <div className="section__quantity">
-                                    <div className="incIcon" onClick={()=>dispatch({type:'changeCount',payload:{operation:'increment',id: id}})}>
-                                        <FaChevronUp />
-                                    </div>
-                                    <div className="quantity">
-                                        {count}
-                                    </div>
-                                    <div className="decIcon" onClick={()=>dispatch({type:'changeCount',payload:{operation:'decrement',id: id}})}>
-                                        <FaChevronDown />
-                                    </div>
-
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className="cart__total">
-                <div className="section__total">
-                    <div className='title'>total</div>
-                    <div className='totalAmout'>$ {totalAmount.toFixed(2)}</div>
+            {
+                cart.length === 0 && <div className="emtpyCart">
+                    <div className="cart-msg">
+                        your cart is empty!
+                    </div>
                 </div>
-            </div>
+            }
+            {   
+                cart.length !== 0 && <>
+                    <div className="cart__details">
+                        {
+                            cart.map(item => {
+                                const {id, title, price, count} = item;
+                                return (
+                                    <div className="section" key={id}>
+                                        <div className="section__title">
+                                            {title}
+                                        </div>
+                                        <div className="section__price">
+                                            $ {price}
+                                        </div>
+                                        <div className="section__removeItem" onClick={()=>dispatch({type:'removeFromCart',payload: {id}})}>
+                                        remove
+                                        </div>
+                                        <div className="section__quantity">
+                                            <div className="incIcon" onClick={()=>dispatch({type:'changeCount',payload:{operation:'increment',id: id}})}>
+                                                <FaChevronUp />
+                                            </div>
+                                            <div className="quantity">
+                                                {count}
+                                            </div>
+                                            <div className="decIcon" onClick={()=>dispatch({type:'changeCount',payload:{operation:'decrement',id: id}})}>
+                                                <FaChevronDown />
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="cart__total">
+                        <div className="section__total">
+                            <div className='title'>total</div>
+                            <div className='totalAmout'>$ {totalAmount.toFixed(2)}</div>
+                        </div>
+                    </div>
+                </>
+            }
         </div>
     )
 }
