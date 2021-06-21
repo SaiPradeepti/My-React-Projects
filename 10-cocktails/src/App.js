13f3aa3468a0d5509707d2f749c33464868d490e
Sai Pradeepti <saipradeepti14@gmail.com>
2021-06-21 15:32:15 +0530

    Adding Navbar to cocktail project

--- 10-cocktails/src/App.js
+++ 10-cocktails/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// importing pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Error from './pages/Error'
+import SingleCocktail from './pages/SingleCocktail'
+// importing components
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      hello
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/cocktail/:id'>
+          <SingleCocktail />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
--- 10-cocktails/src/context.js
+++ 10-cocktails/src/context.js
@@ -5,7 +5,7 @@ const AppContext = React.createContext();
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value={hello}>{children}</AppContext.Provider>
+    return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
 
 export const useGlobalContext = () => {
