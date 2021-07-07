88590d5bea73d6fe791f4638dc1c28925c7fa8b0
Sai Pradeepti <saipradeepti14@gmail.com>
2021-07-07 09:01:39 +0530

    SingleCocktail

--- 10-cocktails/src/components/SearchForm.js
+++ 10-cocktails/src/components/SearchForm.js
@@ -1,32 +1,33 @@
-import React,{ useEffect} from 'react'
+import React from 'react'
 import { useGlobalContext } from '../context'
+export default function SearchForm() {
+  const { setSearchTerm } = useGlobalContext()
+  const searchValue = React.useRef('')
 
-const SearchForm = () => {
-  const { setSearchTerm } = useGlobalContext();
-  const searchValue = React.useRef('');
-
-  useEffect(()=> {
-    searchValue.current.focus();
+  React.useEffect(() => {
+    searchValue.current.focus()
   }, [])
 
-  const searchCocktail = () => {
-    setSearchTerm(searchValue.current.value);
+  function searchCocktail() {
+    setSearchTerm(searchValue.current.value)
   }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault()
   }
-
   return (
     <section className='section search'>
-      <form className="search-form" onSubmit={handleSubmit}>
-        <div className="form-control">
-          <label htmlFor="name">search your favorite cocktail</label>
-          <input type='text' id='name' onChange={searchCocktail} ref={searchValue} />
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
         </div>
       </form>
     </section>
   )
 }
-
-export default SearchForm
--- 10-cocktails/src/useFetch.js
+++ 10-cocktails/src/useFetch.js
@@ -1,46 +1,26 @@
 import React,{ useState, useEffect } from 'react'
 
-export const useFetch = (url,searchTerm) => {
+export const useFetch = (url) => {
     const [loading,setLoading] = useState(true);
     const [error,setError] = useState(null);
-    const [cocktails,setCocktails] = useState([]);
+    const [data,setData] = useState([]);
 
     useEffect(() => {
-
+        
         setTimeout(()=>{
             setLoading(true);
-            
-            fetch(`${url}${searchTerm}`)
+            fetch(url)
                 .then(response =>{
                     if(!response.ok)
                         throw Error("Couldn't fetch the dtata from the source")
                     return response.json()
                 })
-                .then(data => {
-                    const { drinks } = data;
-                    if(drinks){
-                        const newCocktails = drinks.map((item) => {
-                            const {
-                                idDrink,
-                                strDrink,
-                                strDrinkThumb,
-                                strAlcoholic,
-                                strGlass,
-                            } = item
-
-                            return {
-                                id: idDrink,
-                                name: strDrink,
-                                image: strDrinkThumb,
-                                info: strAlcoholic,
-                                glass: strGlass,
-                            }
-                        })
-                        setCocktails(newCocktails);
-                    }
-                    else{
-                        setCocktails([]);
-                    }
+                .then(jsonData => {
+                        console.log(jsonData);
+                        setData(jsonData);
+                    // else{
+                    //     setCocktails([]);
+                    // }
                     setLoading(false);
                 })
                 .catch(error => {
@@ -49,7 +29,7 @@ export const useFetch = (url,searchTerm) => {
                 })
         },1000);
         
-    }, [url,searchTerm])
+    }, [url])
 
-    return { loading, error, cocktails };
+    return { loading, error, data };
 }
