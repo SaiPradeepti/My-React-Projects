45ecbe87f24343d275e629555b18f40d03cf8fd3
Sai Pradeepti <saipradeepti14@gmail.com>
2021-07-08 11:25:16 +0530

    README.md

--- 10-cocktails/src/pages/SingleCocktail.js
+++ 10-cocktails/src/pages/SingleCocktail.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Loading from '../components/Loading'
 import { useParams, Link } from 'react-router-dom'
-import { useFetch } from '../useFetch'
 const url = 'www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
@@ -13,39 +12,41 @@ const SingleCocktail = () => {
         setLaoding(true);
         async function getCocktail() {
             try{
-                const response = await fetch(`${url}${id}`);
+                const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
                 const data = await response.json();
                 if(data.drinks){
                     const {
-            strDrink: name,
-            strDrinkThumb: image,
-            strAlcoholic: info,
-            strCategory: category,
-            strGlass: glass,
-            strInstructions: instructions,
-            strIngredient1,
-            strIngredient2,
-            strIngredient3,
-            strIngredient4,
-            strIngredient5,
-          } = data.drinks[0]
-          const ingredients = [
-            strIngredient1,
-            strIngredient2,
-            strIngredient3,
-            strIngredient4,
-            strIngredient5,
-          ]
-          const newCocktail = {
-            name,
-            image,
-            info,
-            category,
-            glass,
-            instructions,
-            ingredients,
-          }
-          setCocktail(newCocktail)
+                    strDrink: name,
+                    strDrinkThumb: image,
+                    strAlcoholic: info,
+                    strCategory: category,
+                    strGlass: glass,
+                    strInstructions: instructions,
+                    strIngredient1,
+                    strIngredient2,
+                    strIngredient3,
+                    strIngredient4,
+                    strIngredient5,
+                } = data.drinks[0]
+                const ingredients = [
+                    strIngredient1,
+                    strIngredient2,
+                    strIngredient3,
+                    strIngredient4,
+                    strIngredient5,
+                ]
+                const newCocktail = {
+                    name,
+                    image,
+                    info,
+                    category,
+                    glass,
+                    instructions,
+                    ingredients,
+                }
+                setCocktail(newCocktail)
+                console.log(newCocktail)
+                console.log(cocktail)
                 }
                 else{
                     setCocktail(null);
@@ -57,7 +58,8 @@ const SingleCocktail = () => {
             }
         }
         getCocktail();
-    },[id])
+        setLaoding(false);
+    },[id,cocktail])
     
     if(loading){
         return <Loading />
