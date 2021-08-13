500db49853f122c625ea2abba3ccab0272aa958d
Sai Pradeepti <saipradeepti14@gmail.com>
2021-08-14 01:01:26 +0530

    Adding Markdown functionality

--- 11-markdown-preview/src/App.js
+++ 11-markdown-preview/src/App.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+
 
 const App = () => {
+//     const [markdown,setMarkdown] = useState(
+//         `# hi\n## hi\n[google.com](https://google.com)\n\n\`\`\`<h1>Hello World</h1>\`\`\`
+//         # Welcome to my React Markdown Previewer!
+
+// ## This is a sub-heading...
+// ### And here's some other cool stuff:
+
+// Heres some code, \`<div></div>\`, between 2 backticks.
+
+// \`\`\`
+// // this is multi-line code:
+
+// // function anotherExample(firstLine, lastLine) {
+// //   if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+// //     return multiLineCode;
+// //   }
+// // }'
+// \`\`\`
+
+// You can also make text **bold**... whoa!
+// Or _italic_.
+// Or... wait for it... **_both!_**
+// And feel free to go crazy ~~crossing stuff out~~.
+
+// There's also [links](https://www.freecodecamp.org), and
+// > Block Quotes!
+
+// And if you want to get really crazy, even tables:
+
+// Wild Header | Crazy Header | Another Header?
+// ------------ | ------------- | -------------
+// Your content can | be here, and it | can be here....
+// And here. | Okay. | I think we get it.
+
+// - And of course there are lists.
+//   - Some are bulleted.
+//      - With different indentation levels.
+//         - That look like this.
+
+
+// 1. And there are numbered lists too.
+// 1. Use just 1s if you want!
+// 1. And last but not least, let's not forget embedded images:
+
+// ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+//         `
+//         );
+const [markdown,setMarkdown] = useState('# hi');
     return (
-        <div>
-            hello
+        <div className='container'>
+            <section className='text'>
+                <textarea value={markdown} name="markdownText" id="markdownText" cols="30" rows="10" onChange={(e) => setMarkdown(e.target.value)} id='editor'></textarea>
+            </section>
+            <section className='markdownText' >
+               <div id="preview">
+                   <ReactMarkdown>{markdown}</ReactMarkdown>
+                </div>
+            </section>
         </div>
     )
 }
--- 12-notifications/src/data.js
+++ 12-notifications/src/data.js
@@ -4,7 +4,7 @@ export const data = [
         title: 'Apple iPhone 13 to be released in third week of September',
         imgLink: 'https://cdn.pocket-lint.com/r/s/660x/assets/images/120810-phones-news-buyer-s-guide-upcoming-smartphones-image29-rt3nwpuh9j-jpg.webp?v1',
         link:'https://www.indiatoday.in/technology/features/story/iphone-13-to-launch-next-month-specs-features-design-cameras-india-price-and-all-that-we-know-so-far-1837203-2021-08-05',
-        timestamp: 1628397600,
+        timestamp: 1628431500,
 
     },
     {
@@ -12,7 +12,7 @@ export const data = [
         title: 'Google Pixel 6 expected to be released in October 2021',
         imgLink: 'https://cdn.pocket-lint.com/r/s/660x/assets/images/120810-phones-news-buyer-s-guide-upcoming-smartphones-image60-fg5kzb7ygs-jpg.webp?v1',
         link:'https://www.tomsguide.com/news/google-pixel-6-release-date-price-tensor-chip-specs-and-news',
-        timestamp: 1628397720,
+        timestamp: 1628431530,
 
     },
     {
@@ -20,7 +20,7 @@ export const data = [
         title: 'Samsung Galaxy Z Fold 3, expected to launch on 11 August 2021',
         imgLink: 'https://cdn.pocket-lint.com/r/s/660x/assets/images/120810-phones-news-buyer-s-guide-upcoming-smartphones-image39-ei7bcnpztu.JPG?v1',
         link:'https://gadgets.ndtv.com/mobiles/news/samsung-galaxy-z-fold-flip-3-complete-specifications-renders-leak-foldable-august-11-launch-unpacked-2504979',
-        timestamp: 1628397840,
+        timestamp: 1628431560,
 
     },
 ];
