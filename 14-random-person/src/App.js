ff966e62c8e9f0b037df3e3de451a343fe6e04fa
Sai Pradeepti <saipradeepti14@gmail.com>
2021-10-10 01:40:14 +0530

    Adding Starter files for random person project

---
+++ 14-random-person/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa';
+
+const url = 'https://randomuser.me/api/';
+
+const defaultImage = 'https://randomuser.me/api/portraits/med/men/96.jpg';
+
+const App = () => {
+  return (
+    <h2>Random person starter</h2>
+  )
+}
+
+export default App
