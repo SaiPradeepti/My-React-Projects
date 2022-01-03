b5d048943edb689f6774a4781f57c40b5079bd1d
Sai Pradeepti <saipradeepti14@gmail.com>
2022-01-03 17:15:19 +0530

    Adding useFetch Custom Hook to pagination project

--- 16-pagination/src/App.js
+++ 16-pagination/src/App.js
@@ -1,20 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import Users from './Users'
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
+import {useFetch} from './useFetch'
 const paginationBtns  =[1,2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const App = () => {
-  const [users,setUsers] = useState([]);
   const [displayindex,setDisplayIndex] = useState(0);
-  const [loading,setLoading] = useState(true)
-
-  const getUsers = async () => {
-    const data = await fetch(url);
-    const users = await data.json();
-    setUsers(users)
-    setTimeout(() => {setLoading(false)}, 2000)
-    return users;
-  }
+  const {loading, data} = useFetch();
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -34,13 +25,7 @@ const App = () => {
       setDisplayIndex(0)
     else
       setDisplayIndex(displayindex => parseInt(displayindex) + 1)
-  }
-
-  useEffect(() =>{
-    getUsers();
-  },[]);
-
-  
+  }  
   
   return (
     <div className="main">
@@ -48,12 +33,11 @@ const App = () => {
       <h1 className='title'>Pagination</h1>
       <div className="underline"></div>
     </div>
-    <Users users={users} displayindex={displayindex} loading={loading}/>
+    <Users data={data} displayindex={displayindex} loading={loading}/>
     <div className="pagination">
       <div className="prev" onClick={handlePrevClick}>Prev</div>
       {
         paginationBtns.map((item,index) => {
-          console.log(index,displayindex)
           return (
             <div key={index} className={`btn ${index === parseInt(displayindex) ? 'active' : ''}`} data-label={index} onClick={handleClick}>
               {item}
--- 16-pagination/src/Users.js
+++ 16-pagination/src/Users.js
@@ -1,19 +1,13 @@
 import React from 'react'
 
-const Users = ({users,displayindex,loading}) => {
+const Users = ({data,displayindex,loading}) => {
     const i = displayindex;
-    console.log(i)
-    const displayUsers = users.map((item,index) => {
+    const users = data.map((item,index) => {
         if(index >= (i*10) && index <= (parseInt(i)+1)*10-1)
             return item;
         else
         return null;
     })
-    // console.log(displayUsers)
-    
-    const viewProfile = (profile) => {
-
-    }
 
     if(loading){
     return <div className='loading'><h1>Loading...</h1></div>
@@ -22,14 +16,14 @@ const Users = ({users,displayindex,loading}) => {
     return (
         <div className='cardContainer'>
             {
-                displayUsers.map( (item,index) => {
+                users.map( (item,index) => {
                     if(item !== null){
                         const {avatar_url:image,login:name,html_url:profile} = item;
                         return (
                             <div className="card" key={index}>
                                 <img src={image} alt={name} className='card-image'/>
                                 <h1 className='card-name'>{name}</h1>
-                                <button onClick={() => viewProfile(profile)} className='card-btn'>
+                                <button className='card-btn'>
                                     <a href={profile} target='_blank' rel="noreferrer">view profile</a></button>
                             </div>
                         )
